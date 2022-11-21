679eab1843fb0d032cdd02e0ccf7fcc8b13cf345
alineol <allinne.oliveira.ol@gmail.com>
2022-11-21 18:58:13 -0300

    feat: cria inputs para roda de pessoa cuidadora e pets

---
+++ src/GeoPetWebApi/Controllers/inputs/PessoaCuidadoraInput.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projetoFinal.Controllers.inputs
+{
+    public class PessoaCuidadoraInput
+    {
+        [Column("NOME")]
+        public string Nome { get; set; }
+        [Column("EMAIL")]
+        public string Email { get; set; }
+        [Column("SENHA", TypeName = "varchar(10)")]
+        [MinLength (6)]
+        [MaxLength(10)]
+        public string senha { get; set; }
+        [Column(TypeName = "varchar(11)")]
+        [MinLength(11)]
+        [MaxLength(11)]
+        public string CPF { get; set; }
+        [Column(TypeName = "varchar(8)")]
+        [MinLength(8)]
+        [MaxLength(8)]
+        public string CEP { get; set; }
+    }
+}
---
+++ src/GeoPetWebApi/Controllers/inputs/PetInput.cs
@@ -0,0 +1,23 @@
+﻿using projetoFinal.db.Models.PessoaCuidadora;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projetoFinal.Controllers.inputs
+{
+    public class PetInput
+    {
+        [Column("NOME")]
+        public string Nome { get; set; }
+        [Column("PESO")]
+        public decimal Peso { get; set; }
+        [Column("PESSOA_CUIDADORA")]
+        public virtual PessoaCuidadoraModel PessoaCuidadora { get; set; }
+        [Column("HASH_LOCALIZACAO")]
+        public string HashLocalizacao { get; set; }
+        [Column("IDADE")]
+        public int Idade { get; set; }
+        [Column("RACA")]
+        public string Raca { get; set; }
+        [Column("PORTE")]
+        public string Porte { get; set; }
+    }
+}
