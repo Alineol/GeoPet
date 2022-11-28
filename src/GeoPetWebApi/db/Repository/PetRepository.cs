079ad8ee45227d14dcd7d81ea9dedcdfa9f1d787
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-11-28 19:06:50 -0300

    fix: get byId

--- src/GeoPetWebApi/db/Repository/PetRepository.cs
+++ src/GeoPetWebApi/db/Repository/PetRepository.cs
@@ -19,7 +19,7 @@ namespace projetoFinal.db.Repository
             return _context.SaveChanges();
         }
 
-        public IEnumerable<PetModel> GetAll() {
+        public IEnumerable<PetModel>? GetAll() {
             var result = _context.Pets.Select(pet => new PetModel()
             {
                 Nome = pet.Nome,
@@ -38,8 +38,21 @@ namespace projetoFinal.db.Repository
 
         public PetModel? GetById(int id)
         {
-            var result = _context.Pets.Where(p => p.Id == id).FirstOrDefault();
-            
+            var pet = _context.Pets.Where(p => p.Id == id).FirstOrDefault();
+
+            var result = new PetModel()
+            {
+                Nome = pet.Nome,
+                Peso = pet.Peso,
+                Idade = pet.Idade,
+                Raca = pet.Raca,
+                Porte = pet.Porte,
+                Status = pet.Status,
+                Id = pet.Id,
+                PessoaCuidadora = pet.PessoaCuidadora,
+                HashLocalizacao = pet.HashLocalizacao,
+            };
+
             return result;
         }
 
