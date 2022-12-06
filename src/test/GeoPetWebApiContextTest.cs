0600894b7bad111bf61f6777c1d85b133935ed82
Aline Oliveira <allinne.oliveira.ol@gmail.com>
2022-12-06 11:26:18 -0300

    test: cria contexto de teste e testa aplicação

---
+++ src/test/GeoPetWebApiContextTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using projetoFinal.db.Models.PessoaCuidadora;
+using projetoFinal.db.Models.Pets;
+using GeoPetWebApi.db.Repository.video_portal.Repository;
+
+namespace src
+{
+    public class GeoPetWebApiContextTest: DbContext, IGeoPetContext
+    {
+        public DbSet<PessoaCuidadoraModel> PessoasCuidadoras { get; set; }
+        public DbSet<PetModel> Pets { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var serviceProvider = new ServiceCollection()
+           .AddEntityFrameworkInMemoryDatabase()
+           .BuildServiceProvider();
+
+            optionsBuilder.UseInMemoryDatabase("GeoPetDB").UseInternalServiceProvider(serviceProvider);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PetModel>().HasOne(p => p.PessoaCuidadora).WithMany(a => a.Pets).HasForeignKey("PESSOA_CUIDADORA").OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
