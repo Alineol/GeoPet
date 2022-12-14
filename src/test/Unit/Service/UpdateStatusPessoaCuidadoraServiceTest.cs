e22e0c90d3f9ba0b193d57a8a972ef6b0d7d6df6
Aline Oliveira <allinne.oliveira.ol@gmail.com>
2022-12-14 16:46:11 -0300

    test: pessoa cuidadpra updateStatus service

---
+++ src/test/Unit/Service/UpdateStatusPessoaCuidadoraServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using projetoFinal.Services;
+using static src.Unit.helpers.GeneratePessoaCuidadoraHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using projetoFinal.db.Models.PessoaCuidadora;
+using FluentAssertions;
+
+namespace src.Unit.Service
+{
+    public class UpdateStatusPessoaCuidadoraServiceTest
+    {
+        private readonly PessoaCuidadoraService _service = GeneratePessoaCuidadoraService();
+
+
+        [Fact]
+        public async void ShouldUpadteWithSucess()
+        {
+            PessoaCuidadoraModel input = new PessoaCuidadoraModel()
+            {
+                CEP = "20020000",
+                Nome = "string",
+                Senha = "string",
+                Email = "string",
+                Status = true
+            };
+            
+            await _service.CreatePessoaCuidadora(input);
+
+            var output = _service.UpdateStatusPessoaCuidadora(input.Email);
+
+            output.RowsAffected.Should().Be(1);
+            output.SucessMessage.Should().Be("Pessoa cuidadora desativada.");
+
+            var output2 = _service.UpdateStatusPessoaCuidadora(input.Email);
+
+            output2.RowsAffected.Should().Be(1);
+            output2.SucessMessage.Should().Be("Pessoa cuidadora ativada.");
+
+        }
+    }
+}
